8acf0cfb55bf91a5f7efcd4a8b6efe968ba4acc5
Nikhil Sarwara <nikhil4818.be22@chitkara.edu.in>
2024-04-25 00:18:15 +0530

    add generate qr code svg api

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import QRCodeGenerator from '../utils/qrcode';
 
 export const config = {
   runtime: 'edge'
@@ -7,8 +8,21 @@ export const config = {
 
 const app = new Hono().basePath('/api')
 
+const generator = new QRCodeGenerator();
+
 app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
+  return c.json({ message: "Hello Hono!" });
+})
+
+// Generate QR code SVG
+app.get('/generate', async (c) => {
+  const { text } = c.req.query();
+  try {
+    const response = await generator.generateQRCode(text);
+    return c.json({ message: "Success", data: response });
+  } catch (err) {
+    return c.json({ error: "Failed to generate QR code." });
+  }
 })
 
 export default handle(app)
