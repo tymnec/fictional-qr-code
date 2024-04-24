1da83fa4f1b1b7af611f16a7a076159d95c96e95
Nikhil Sarwara <nikhil4818.be22@chitkara.edu.in>
2024-04-25 00:17:00 +0530

    add qrcode class

---
+++ utils/qrcode.ts
@@ -0,0 +1,16 @@
+import * as qr from "qrcode";
+
+class QRCodeGenerator {
+  async generateQRCode(text: string) {
+    try {
+      // Generate QR code
+      const qrData = await qr.toString(text);
+      return qrData
+    } catch (err) {
+      console.error("Error generating QR code:", err);
+      throw err;
+    }
+  }
+}
+
+export default QRCodeGenerator;
